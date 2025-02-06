55d7ce09f48d45b4eff981f76100b20c4b9c1913
WearyTravler <limbo_shell@protonmail.com>
2025-02-06 11:26:28 -0600

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
+        
+
         System.out.print("$ ");
+
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
-
+        do {
+            System.out.println(input + ": command not found");
+            System.out.print("$ ");
+            input = scanner.nextLine();
+            
+        } while (!input.matches(""));
+        
+        scanner.close();
     }
 }
